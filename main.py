ca827b59216abdaa5193a50f8dd01079d1ff8ecd
joelgerge-web <joelgeorge98765@gmail.com>
2024-07-12 15:23:48 +0530

    chore: Remove unused dependencies and code files

--- main.py
+++ main.py
@@ -1,100 +1,118 @@
+import streamlit as st
+import os
+import tempfile
+import subprocess
 from langchain_community.document_loaders import PyPDFLoader
-from langchain_community.document_loaders import UnstructuredPDFLoader
-from langchain_community.document_loaders import OnlinePDFLoader
 from langchain_community.embeddings import OllamaEmbeddings
-from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import Chroma
 from langchain.prompts import ChatPromptTemplate, PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from langchain_community.chat_models import ChatOllama
 from langchain_core.runnables import RunnablePassthrough
 from langchain.retrievers.multi_query import MultiQueryRetriever
-import streamlit as st
-import os
-import time
-current_dir = os.getcwd()
-save_file_name = None  # Initialize the global variable
 
-st.title("PDF Reader")
-st.write("Upload a PDF file to read its content.")
-uploaded_file = st.file_uploader("Choose a file to upload", type=['txt', 'pdf', 'docx'])
+def model_loader(file):
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".pdf") as tmp_file:
+        tmp_file.write(file.read())
+        tmp_file_path = tmp_file.name
 
-file_path = uploaded_file
-loader = PyPDFLoader(file_path)
-data = loader.load()
-st.success(f"Successfully uploaded")
+    loader = PyPDFLoader(tmp_file_path)
+    data = loader.load()
+    st.success(f"Successfully uploaded {file.name}")
 
+    # Split and chunk
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=7500, chunk_overlap=100)
+    chunks = text_splitter.split_documents(data)
 
-# Split and chunk 
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=7500, chunk_overlap=100)
-chunks = text_splitter.split_documents(data)
+    # Add to vector database
+    vector_db = Chroma.from_documents(
+        documents=chunks, 
+        embedding=OllamaEmbeddings(model="nomic-embed-text", show_progress=True),
+        collection_name="local-rag"
+    )
 
-# Add to vector database
-vector_db = Chroma.from_documents(
-    documents=chunks, 
-    embedding=OllamaEmbeddings(model="nomic-embed-text",show_progress=True),
-    collection_name="local-rag"
-)
+    # LLM from Ollama
+    local_model = "llama3"
+    llm = ChatOllama(model=local_model)
 
+    QUERY_PROMPT = PromptTemplate(
+        input_variables=["question"],
+        template="""You are an AI language model assistant. Your task is to generate five
+        different versions of the given user question to retrieve relevant documents from
+        a vector database. By generating multiple perspectives on the user question, your
+        goal is to help the user overcome some of the limitations of the distance-based
+        similarity search. Provide these alternative questions separated by newlines.
+        Original question: {question}"""
+    )
 
-# LLM from Ollama
-local_model = "llama3"
-llm = ChatOllama(model=local_model)
+    # MultiQueryRetriever
+    retriever = MultiQueryRetriever.from_llm(
+        vector_db.as_retriever(), 
+        llm,
+        prompt=QUERY_PROMPT
+    )
 
-QUERY_PROMPT = PromptTemplate(
-    input_variables=["question"],
-    template="""You are an AI language model assistant. Your task is to generate five
-    different versions of the given user question to retrieve relevant documents from
-    a vector database. By generating multiple perspectives on the user question, your
-    goal is to help the user overcome some of the limitations of the distance-based
-    similarity search. Provide these alternative questions separated by newlines.
-    Original question: {question}""",
-)
+    # RAG prompt
+    template = """Answer the question based ONLY on the following context:
+    {context}
+    Question: {question}
+    """
 
-# MultiQueryRetriever
-retriever = MultiQueryRetriever.from_llm(
-    vector_db.as_retriever(), 
-    llm,
-    prompt=QUERY_PROMPT
-)
+    prompt = ChatPromptTemplate.from_template(template)
 
-# RAG prompt
-template = """Answer the question based ONLY on the following context:
-{context}
-Question: {question}
-"""
+    # Chain
+    chain = (
+        {"context": retriever, "question": RunnablePassthrough()}
+        | prompt
+        | llm
+        | StrOutputParser()
+    )
 
-prompt = ChatPromptTemplate.from_template(template)
+    return chain
 
-# Chain
-chain = (
-    {"context": retriever, "question": RunnablePassthrough()}
-    | prompt
-    | llm
-    | StrOutputParser()
-)
+def main():
+    try:
+        result = subprocess.run(
+            ["ollama", "pull", "nomic-embed-text"],
+            check=True,
+            text=True
+        )
+        st.success("Model pulled successfully.")
+    except subprocess.CalledProcessError as e:
+        st.error(f"An error occurred while pulling the model: {e}")
+        return
 
+    st.title("PDF Chatbot")
+    st.write("Upload a PDF file")
+    file = st.file_uploader("Choose a file to upload", type=['pdf'])
 
-st.title("Ask your questions here!")
+    if file is not None:
+        chain = model_loader(file)
+        st.title("Ask your questions here!")
 
-if "messages" not in st.session_state:
-    st.session_state.messages = []
+        if "messages" not in st.session_state:
+            st.session_state.messages = []
 
-# Display chat messages from history on app rerun
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
+        # Display chat messages from history on app rerun
+        for message in st.session_state.messages:
+            with st.chat_message(message["role"]):
+                st.markdown(message["content"])
 
+        if prompt := st.chat_input("Message Chatbot..."):
+            # Add user message to chat history
+            st.session_state.messages.append({"role": "user", "content": prompt})
+            # Display user message in chat message container
+            with st.chat_message("user"):
+                st.markdown(prompt)
 
-if prompt := st.chat_input("Message Chatbot..."):
-    # Add user message to chat history
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    # Display user message in chat message container
-    with st.chat_message("user"):
-        st.markdown(prompt)
+            # Display assistant response in chat message container
+            with st.chat_message("assistant"):
+                response = chain.invoke({"question": prompt})
+                st.markdown(response)
 
-    # Display assistant response in chat message container
-    with st.chat_message("assistant"):
-        response = st.write_stream(chain.invoke(prompt))
-    # Add assistant response to chat history
-    st.session_state.messages.append({"role": "assistant", "content": response})
+            # Add assistant response to chat history
+            st.session_state.messages.append({"role": "assistant", "content": response})
+
+if __name__ == "__main__":
+    main()
