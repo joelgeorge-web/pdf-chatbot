2e0f62aafed8589a73479688b0d2f9ebc84f6c98
joelgerge-web <joelgeorge98765@gmail.com>
2024-07-15 23:38:39 +0530

    chore: Update model_loader function in main.py

--- main.py
+++ main.py
@@ -38,11 +38,8 @@ def model_loader(file):
 
     QUERY_PROMPT = PromptTemplate(
         input_variables=["question"],
-        template="""You are an AI language model assistant. Your task is to generate five
-        different versions of the given user question to retrieve relevant documents from
-        a vector database. By generating multiple perspectives on the user question, your
-        goal is to help the user overcome some of the limitations of the distance-based
-        similarity search. Provide these alternative questions separated by newlines.
+        template="""You are an AI language model assistant. Your task is to generate an answer to
+          the given user question to retrieve relevant documents from a vector database.
         Original question: {question}"""
     )
 
